dfdbfb0885582546b0323387ae5f153386c63ce7
Diego Lasa <dilasgoi@protonmail.com>
2023-07-06 13:09:24 +0200

    Style changes, add postfinalization message, add parallel option for the installation files

--- bin/eb_wrapper.sh
+++ bin/eb_wrapper.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+# Trap the ERR signal
+trap 'handle_error $LINENO $?' ERR
+
 # Set absolute path to the main script
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 # Source utility scripts
-source "$SCRIPT_DIR/../lib/utils.sh"
+source "${SCRIPT_DIR}/../lib/utils.sh"
 
 # Initialize the dryrun variable to 0 (false)
 DRYRUN=0
@@ -32,28 +35,35 @@ done
 shift $((OPTIND -1))
 
 # Check if a filename and username have been provided
-check_filename "$FILENAME" || exit 1
-check_username "$USERNAME" || exit 1
+check_filename "${FILENAME}" || exit 1
+check_username "${USERNAME}" || exit 1
 
 # Check if the file exists
 check_file_exists "$SCRIPT_DIR" "$FILENAME" || exit 1
 
 # Load configuration
-CONF_FILE="$SCRIPT_DIR/../conf/settings.yaml"
-BUILD_PATH=$(yq e '.buildpath' $CONF_FILE)
-HIDE_DEPS=$(yq e '.hide_deps' $CONF_FILE)
+CONF_FILE="${SCRIPT_DIR}/../conf/settings.yaml"
+BUILD_PATH=$(yq e '.buildpath' ${CONF_FILE})
+HIDE_DEPS=$(yq e '.hide_deps' ${CONF_FILE})
 
 # Parse the YAML file
-parse_yaml "$SCRIPT_DIR" "$FILENAME"
+parse_yaml "${SCRIPT_DIR}" "${FILENAME}"
+
+EB_COMMAND="eb --buildpath=${BUILD_PATH} --hide-deps=${HIDE_DEPS} ${EASYCONFIG}.eb"
+
+# If PARALLEL is not empty, add --parallel option
+if [ -n "${PARALLEL}" ]; then
+    EB_COMMAND+=" --parallel=${PARALLEL}"
+fi
 
 # If not a dry run, prepare for logging and run eb
-if [ $DRYRUN -eq 0 ]; then
+if [ ${DRYRUN} -eq 0 ]; then
     # Prepare for logging
     TIMESTAMP="$(date +%Y%m%d%H%M%S)"
     LOGFILE="${SCRIPT_DIR}/../logs/${EASYCONFIG}-${TIMESTAMP}.log"
 
     # Register the start time and the easyconfig name
-    echo "$(date): Installation started for ${EASYCONFIG} by $USERNAME" | tee -a "${LOGFILE}"
+    echo "$(date): Installation started for ${EASYCONFIG} by ${USERNAME}" | tee -a "${LOGFILE}"
 
     # Log the content of the YAML file
     echo "YAML file content:" | tee -a "${LOGFILE}"
@@ -61,17 +71,22 @@ if [ $DRYRUN -eq 0 ]; then
 
     # Log the SHA256 hash of the YAML file
     echo "SHA256 hash of the YAML file:" | tee -a "${LOGFILE}"
-    sha256sum "${SCRIPT_DIR}/../installation_files/$FILENAME" | tee -a "${LOGFILE}"
+    sha256sum "${SCRIPT_DIR}/../installation_files/${FILENAME}" | tee -a "${LOGFILE}"
 
     # Run eb with specific buildpath and hidden dependencies, and capture the exit status
-    eb --buildpath=${BUILD_PATH} --hide-deps=${HIDE_DEPS} ${EASYCONFIG}.eb -r
+    ${EB_COMMAND} -r
     EXIT_STATUS=$?
 
-    # Register the end time and the exit status
-    echo "$(date): Installation ended for $EASYCONFIG, exit status: ${EXIT_STATUS}" | tee -a "${LOGFILE}"
+    # Call the function to check the exit status
+    check_exit_status "${EXIT_STATUS}" \
+                      "${EASYCONFIG}" \
+                      "${LOGFILE}"
 
+    post_execution_summary "${FILENAME}" "${USERNAME}" "${APPLICATION}" "${VERSION}" "${TOOLCHAIN}" \
+                           "${TOOLCHAIN_VERSION}" "${SUFFIX}" "${PARALLEL}" "${EASYCONFIG}" \
+                           "${EB_COMMAND}" "${LOGFILE}"
 else
     # Dry run
-    echo "Performing dry run for $EASYCONFIG"
-    eb --buildpath=${BUILD_PATH} --hide-deps=${HIDE_DEPS} ${EASYCONFIG}.eb -Dr
+    echo "Performing dry run for ${EASYCONFIG}"
+    ${EB_COMMAND} -Dr
 fi
--- lib/utils.sh
+++ lib/utils.sh
@@ -38,13 +38,70 @@ function parse_yaml {
     TOOLCHAIN=$(yq e '.toolchain // ""' $yaml_file)  # Use an empty string if null
     TOOLCHAIN_VERSION=$(yq e '.toolchain_version // ""' $yaml_file)  # Use an empty string if null
     SUFFIX=$(yq e '.suffix // ""' $yaml_file)  # Use an empty string if null
+    PARALLEL=$(yq e '.parallel // ""' $yaml_file)  # Use an empty string if null
     COMMENTS=$(yq e '.comments' $yaml_file)
 
-    # If toolchain or toolchain_version are not specified, do not include them or the following hyphen
-    if [ -z "$TOOLCHAIN" ] && [ -z "$TOOLCHAIN_VERSION" ]; then
-        EASYCONFIG="${APPLICATION}-${VERSION}${SUFFIX}"
-    else
-        EASYCONFIG="${APPLICATION}-${VERSION}-${TOOLCHAIN}-${TOOLCHAIN_VERSION}${SUFFIX}"
+    EASYCONFIG="${APPLICATION}-${VERSION}"
+
+    if [ -n "$TOOLCHAIN" ]; then
+        EASYCONFIG+="-${TOOLCHAIN}"
+    fi
+
+    if [ -n "$TOOLCHAIN_VERSION" ]; then
+        EASYCONFIG+="-${TOOLCHAIN_VERSION}"
+    fi
+
+    if [ -n "$SUFFIX" ]; then
+        EASYCONFIG+="-${SUFFIX}"
     fi
 }
 
+# Function to check the exit status
+function check_exit_status {
+    local exist_status=$1
+    local easyconfig=$2
+    local logfile=$3
+
+    if [ ${exist_status} -ne 0 ]; then
+        echo "$(date): Installation ended with error for ${easyconfig}, exit status: ${exist_status}" | tee -a "${logfile}"
+        echo "Check the log file ${logfile} for details."
+        exit ${exit_status}
+    else
+        echo "$(date): Installation ended successfully for ${easyconfig}, exit status: ${exist_status}" | tee -a "${logfile}"
+    fi
+}
+
+# Function to create a post-execution summary
+function post_execution_summary {
+    local filename=$1
+    local username=$2
+    local application=$3
+    local version=$4
+    local toolchain=$5
+    local toolchain_version=$6
+    local suffix=$7
+    local parallel=$8
+    local easyconfig=$9
+    local eb_command=${10}
+    local logfile=${11}
+
+    echo "==== Post-execution Summary ====" | tee -a "${logfile}"
+    echo "Installation File: ${filename}" | tee -a "${logfile}"
+    echo "Username: ${username}" | tee -a "${logfile}"
+    echo "Application: ${application}" | tee -a "${logfile}"
+    echo "Version: ${version}" | tee -a "${logfile}"
+    if [ -n "$toolchain" ]; then echo "Toolchain: ${toolchain}" | tee -a "${logfile}"; fi
+    if [ -n "$toolchain_version" ]; then echo "Toolchain Version: ${toolchain_version}" | tee -a "${logfile}"; fi
+    if [ -n "$suffix" ]; then echo "Suffix: ${suffix}" | tee -a "${logfile}"; fi
+    if [ -n "$parallel" ]; then echo "Parallel: ${parallel}" | tee -a "${logfile}"; fi
+    echo "Easyconfig: ${easyconfig}" | tee -a "${logfile}"
+    echo "Command: ${eb_command}" | tee -a "${logfile}"
+    echo "Log File: ${logfile}" | tee -a "${logfile}"
+}
+
+# Error handling function
+function handle_error {
+    echo "An error occurred on line $1 of the script."
+    echo "Exit status: $2"
+    exit $2
+}
